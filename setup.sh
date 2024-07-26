8b8dc1f6d54aa6bb440eb14ba1ccb7ebec525394
Rob Kutschke <kutschke@fnal.gov>
2024-07-26 15:50:55 -0500

    Add coded needed for AL9/Spack. The code now works on both SL7/UPS and AL9/spack.

--- setup.sh
+++ setup.sh
@@ -14,3 +14,14 @@ mkdir -p Utilities/obj/
 mkdir -p output
 
 export PATH=Run/bin/:$PATH
+
+if [[ "$MU2E_SPACK" == "true" ]]; then
+    # This is not guaranteed to be robust but it works for now
+    # All env variables point into the spack environment
+    export BOOST_INC=${ROOT_INC}
+    export ROOT_INC=${ROOT_INC}/root
+    export BOOST_LIB=`echo $MUSE_LIBRARY_PATH  | awk 'BEGIN{FS=":"}{print $1}'`
+
+    # This is needed at run time since we are not doing rpath linking
+    export LD_LIBRARY_PATH=$BOOST_LIB
+fi
